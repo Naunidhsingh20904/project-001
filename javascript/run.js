fbe9ea03a31fa7606063304cbad11a97f7c97a67
Naunidh Singh <naunidhw09@gmail.com>
2026-01-07 20:03:25 -0500

    js

---
+++ javascript/run.js
@@ -0,0 +1,15 @@
+console.log("mera naam naunidh hai!");
+let radius = 3;
+
+// let use to declare variables
+// const
+// var we can mutate this using
+var age = 24;
+var age = 23;
+var age = 21;
+// but cannot be re-declarfed using let
+
+let age = 23;
+age = 50;
+
+console.log(age);
