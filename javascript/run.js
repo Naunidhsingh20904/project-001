4cb8ccf359013537b0b3fbfee75bd8c22fae635e
Naunidh Singh <naunidhw09@gmail.com>
2026-01-09 19:34:46 -0500

    added Dom

--- javascript/run.js
+++ javascript/run.js
@@ -1,15 +1,28 @@
-console.log("mera naam naunidh hai!");
-let radius = 3;
+function asyncfunc1() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data1... ");
+      resolve("success");
+    }, 4000);
+  });
+}
 
-// let use to declare variables
-// const
-// var we can mutate this using
-var age = 24;
-var age = 23;
-var age = 21;
-// but cannot be re-declarfed using let
+function asyncfunc2() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("data2... ");
+      resolve("success");
+    }, 4000);
+  });
+}
 
-let age = 23;
-age = 50;
-
-console.log(age);
+console.log("fetching data1...");
+let p1 = asyncfunc1();
+p1.then((res) => {
+  console.log(res);
+  console.log("fetching data2... ");
+  let p2 = asyncfunc2();
+  p2.then((res) => {
+    console.log(res);
+  });
+});
