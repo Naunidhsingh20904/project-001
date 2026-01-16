7fdcedcbb2c8549fc08ce87eb175537477423157
Naunidh Singh <naunidhw09@gmail.com>
2026-01-16 16:50:16 -0500

    calculator finish

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,11 @@
-// // const URL = 
+// // const URL =
 // // thte fetch api provides an interfave for fetching resources
 // the fetch() method is used to fetch a resoource.a
 // let promise = fetch (url, [options])
 
+const URL = "https://api.thecatapi.com/v1/images/0XYvRd7oD";
+const getfacts = async () => {
+  console.log("getting data");
+  let response = await fetch(URL);
+  console.log(response); // json formst
+};
